bf7f0e0899ac9c48b8bc159c82bf60f16c52b627
lirui <771725652@qq.com>
2023-01-15 15:03:56 +0800

    first

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # crawlCCFPaper
-crawl CCFA paper by key
+本项目用于爬取2022年所有CCFA类会议和期刊（URL在CCFAURL.txt里，可以进行修改）的相关关键字的论文题目
+
+ 修改crawl.py 里的配置（要爬取的关键词，年份等）,之后
+ ```
+ pip install beautiful4
+ python crawl.py
+ ```
+ 结果默认保存在当前目录下的CCFsearch.txt
---
+++ crawl.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+f = open("CCFAURL.txt", encoding="utf-8") # CCF A 会议和期刊的URL 文档
+key = 'cloud' # 搜索关键词
+year = 2018 # 只要大于year的论文
+filename = 'CCFsearch.txt' # 爬到的年份+题目保存在文件中
+links = f.readlines()
+f.close()
+links = [i for i in links if i != '\n']
+newlinks = []
+for i in links:
+    newlinks.append(i.strip().replace('\n', ''))
+    print(key)
+writeli=""
+with open(filename, 'w') as file_object:
+    writeli += key + "\n"
+    for link in newlinks:
+        jouc = link.split('/')[-3]
+        aname = link.split('/')[-2]
+        searchurl = 'https://dblp.uni-trier.de/search?q={que}%20streamid%3A{jorc}%2F{name}%3A'.format(que=key,jorc=jouc,name=aname)
+        strhtml = requests.get(searchurl)
+        soup=BeautifulSoup(strhtml.text,'html.parser')
+        titles = soup.select('span.title')
+        time = soup.select('span[itemprop="datePublished"]')
+        print(jouc+" "+aname)
+        writeli += jouc+" "+aname+"\n"
+        for i in range(len(titles)):
+            if int(time[i].text) < year:
+                break
+            writeli += time[i].text+" "+titles[i].text+"\n"
+    file_object.write(writeli)
+file_object.close()
